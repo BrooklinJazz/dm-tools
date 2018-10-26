d2f2f4b38de2f84e2731d2aa16dbe70f9da077d3
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-10-26 02:06:01 -0700

    remove boilerplate and add vuex with working examples

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>INDEX</div>
+  <div>
+    INDEX {{list}}
+    <button @click="init()">GET DATA</button>
+    {{monsters}}
+  </div>
 </template>
+
+<script>
+import { mapState, mapMutations, mapActions } from "vuex";
+export default {
+  computed: {
+    ...mapState({
+      monsters: state => state.monsters
+    })
+  },
+  methods: {
+    ...mapActions([
+      'init'
+    ])
+  }
+};
+</script>
---
+++ store/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const state = () => ({
+    monsters: "monsters"
+})
+
+export const mutations = {
+    init(state, monsters) {
+        state.monsters = monsters
+    },
+}
+
+export const actions = {
+    async init({commit}) {
+        const monsters = await axios.get("http://dnd5eapi.co/api/monsters/").then(({data}) => data)
+        commit("init", monsters)
+    }
+}
